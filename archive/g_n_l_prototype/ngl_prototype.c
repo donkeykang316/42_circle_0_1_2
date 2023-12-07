bd40c33ccb6ae9d75e86419a7bed98eb95199592
Someng <kang_an_an@hotmail.de>
2023-12-07 14:40:42 +0100

    almost working, need to handle bigger buffersize

---
+++ archive/g_n_l_prototype/ngl_prototype.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   copy.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 05:56:54 by kaan              #+#    #+#             */
+/*   Updated: 2023/12/07 14:04:46 by kaan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*temp;
+	char		*buffer;
+	char		*line;
+	ssize_t		b_read;
+
+	char		*str;
+	char		*remain;
+	int			i;
+
+	b_read = 1;
+	i = 0;
+	if (fd < 0)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	while (b_read > 0)
+	{
+		b_read = read(fd, buffer, BUFFER_SIZE);
+		buffer[b_read] = '\0';
+		temp = ft_strjoin(temp, buffer);
+		if (ft_strchr(temp, '\n'))
+			break ;
+	}
+	free(buffer);
+	while (temp[i] != '\n' && temp[i] != '\0')
+	{
+		i++;
+	}
+	if (temp[i] == '\n')
+	{
+		str = ft_substr(temp, 0, i + 1);
+		printf("error check:%s\n", temp);
+		remain = ft_substr(temp, i + 1, ft_strlen(temp) + 1);
+		remain[ft_strlen(temp) + 1] = '\0';
+		temp = ft_strdup(remain);
+		line = ft_strdup(str);
+	}
+	else
+		line = ft_strdup(temp);
+	return (line);
+}
+
+int main()
+{
+	const char  *filename = "test";
+	int         fd;
+	int			i = 6;
+	char		*c;
+
+	fd = open(filename, O_RDONLY);
+	while (i > 0)
+	{
+		c = get_next_line(fd);
+		//printf("%s", c);
+		i--;
+	}
+	free (c);
+	close(fd);
+}
+*/
