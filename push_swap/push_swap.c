beb86026273affbdbf07598e7c642e4273cdb7bc
donkeykang316 <kang_an_an@hotmail.de>
2023-12-29 12:47:57 +0100

    stacking function

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,21 +6,16 @@
 /*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 14:13:11 by kaan              #+#    #+#             */
-/*   Updated: 2023/12/29 12:38:36 by kaan             ###   ########.fr       */
+/*   Updated: 2023/12/29 12:47:09 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int ac, char **av)
+void	stacking(t_list **stack_a, int ac, char **av, int i)
 {
-	int			i;
-	t_list		**stack_a;
 	t_list		*new;
 
-	i = 1;
-	stack_a = malloc(ac * sizeof (t_list));
-	*stack_a = NULL;
 	if (ac >= 2)
 	{
 		while (av[i])
@@ -29,8 +24,19 @@ int	main(int ac, char **av)
 			ft_lstadd_back(stack_a, new);
 			i++;
 		}
-	ft_printf("%d\n", ft_atoi((*stack_a)->content));
+		ft_printf("%d\n%d\n", ft_atoi((*stack_a)->content), ft_atoi((*stack_a)->next->content));
 	}
 	free(new);
+}
+
+int	main(int ac, char **av)
+{
+	int			i;
+	t_list		**stack_a;
+
+	i = 1;
+	stack_a = malloc(ac * sizeof (t_list));
+	*stack_a = NULL;
+	stacking(stack_a, ac, av, i);
 	free(stack_a);
 }
