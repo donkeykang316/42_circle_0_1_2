5bcfa6f3d44be8cdd0813f232c852b7bef664582
Kang An <kaan@c4b5c2.42berlin.de>
2024-01-02 13:08:05 +0100

    Happy new year

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -3,18 +3,71 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
+/*   By: kaan <kaan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 14:13:11 by kaan              #+#    #+#             */
-/*   Updated: 2023/12/29 12:47:09 by kaan             ###   ########.fr       */
+/*   Updated: 2024/01/02 13:07:44 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	ft_bool(t_list **stack_a)
+{
+	t_list	*temp;
+
+	temp = *stack_a;
+	while (temp && (temp)->next != NULL)
+	{
+		if (ft_atoi((temp)->content) <= ft_atoi((temp)->next->content))
+			temp = (temp)->next;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+int	get_min(t_list **stack_a)
+{
+	int		min;
+	int		position;
+	t_list	*temp1;
+	t_list	*temp2;
+
+	temp1 = *stack_a;
+	temp2 = *stack_a;
+	position = 0;
+	if (ft_bool(stack_a) == 0)
+	{
+		min = ft_atoi(temp1->content);
+		while (temp1)
+		{
+			if (min >= ft_atoi(temp1->content))
+				min = ft_atoi(temp1->content);
+			temp1 = temp1->next;
+		}
+	}
+	while (ft_atoi(temp2->content) != min)
+	{
+		temp2 = temp2->next;
+		position++;
+	}
+	return (position);
+}
+
+void	ft_ra(t_list **stack_a, int position)
+{
+	// look for the list index and ra
+	while (position >= 0)
+	{
+		position--;
+	}
+}
+
 void	stacking(t_list **stack_a, int ac, char **av, int i)
 {
 	t_list		*new;
+	int			position;
 
 	if (ac >= 2)
 	{
@@ -24,7 +77,7 @@ void	stacking(t_list **stack_a, int ac, char **av, int i)
 			ft_lstadd_back(stack_a, new);
 			i++;
 		}
-		ft_printf("%d\n%d\n", ft_atoi((*stack_a)->content), ft_atoi((*stack_a)->next->content));
+		position = get_min(stack_a);
 	}
 	free(new);
 }
