91851d257371100e612490caa1567f659b13c627
Kang An <kaan@c4b8c1.42berlin.de>
2024-01-04 18:08:57 +0100

    update

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: kaan <kaan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 14:13:11 by kaan              #+#    #+#             */
-/*   Updated: 2024/01/04 13:35:58 by kaan             ###   ########.fr       */
+/*   Updated: 2024/01/04 18:08:42 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,26 @@ void	stacking(t_list **stack_a, int ac, char **av)
 	}
 }
 
+void	push_b(t_list *stack_a, t_list **stack_b)
+{
+	ft_lstadd_back(stack_b, stack_a);
+	free(stack_a);
+	stack_a = stack_a->next;
+}
+
 int	main(int ac, char **av)
 {
 	t_list		*stack_a;
+	t_list		*stack_b;
 
 	stack_a = malloc(sizeof (t_list));
+	stack_b = malloc(sizeof (t_list));
 	stack_a = NULL;
+	stack_b = NULL;
 	stacking(&stack_a, ac, av);
+	push_b(stack_a, &stack_b);
+
+	ft_printf("%d\n", stack_b->content);
+	ft_printf("%d\n", stack_a->content);
+	stack_b = stack_b->next;
 }
