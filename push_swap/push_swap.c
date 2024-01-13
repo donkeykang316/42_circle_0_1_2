b26ea9833e20a28bc38db2bf5d32e4b43e0fe334
donkeykang316 <kang_an_an@hotmail.de>
2024-01-13 18:05:44 +0100

    optmization needed

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,25 +6,70 @@
 /*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 14:13:11 by kaan              #+#    #+#             */
-/*   Updated: 2024/01/12 18:58:53 by kaan             ###   ########.fr       */
+/*   Updated: 2024/01/13 18:04:47 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	rot_check(t_list **stack_a, t_list **stack_b)
+int	case_ra(t_list **stack_a)
 {
-	if ((*stack_a) && (*stack_b))
+	int		i;
+	t_list	*temp;
+
+	i = 1;
+	temp = *stack_a;
+	while (temp->content != get_min(&temp)->content)
 	{
-		if ((*stack_a)->content > ft_lstlast(*stack_a)->content
-			&& (*stack_b)->content < ft_lstlast(*stack_b)->content)
-			rr(stack_a, stack_b);
-		else if ((*stack_a)->content > ft_lstlast(*stack_a)->content
-			&& (*stack_b)->content >= ft_lstlast(*stack_b)->content)
-			ra(stack_a);
-		else if ((*stack_a)->content <= ft_lstlast(*stack_a)->content
-			&& (*stack_b)->content > ft_lstlast(*stack_b)->content)
-			rb((stack_b));
+		ra(stack_a);
+		i++;
+	}
+	return (i);
+}
+
+int	case_rra(t_list **stack_a)
+{
+	int		i;
+	t_list	*temp;
+
+	i = 1;
+	temp = *stack_a;
+	while (temp->content != get_min(&temp)->content)
+	{
+		rra(stack_a);
+		i++;
+	}
+	return (i);
+}
+
+void	apply_ra(t_list **stack_a)
+{
+	while ((*stack_a)->content != get_min(stack_a)->content)
+		ra(stack_a);
+}
+
+void	apply_rra(t_list **stack_a)
+{
+	while ((*stack_a)->content != get_min(stack_a)->content)
+		rra(stack_a);
+}
+
+void	stack_check(t_list **stack_a)
+{
+	int	ra_i;
+	int	rra_i;
+
+	ra_i = case_ra(stack_a);
+	rra_i = case_rra(stack_a);
+	if (ra_i < rra_i)
+	{
+		ft_printf("ra:%d\n", ra_i);
+		apply_ra(stack_a);
+	}
+	else if (ra_i >= rra_i)
+	{
+		ft_printf("rra:%d\n", rra_i);
+		apply_rra(stack_a);
 	}
 }
 
@@ -32,13 +77,30 @@ void	push_b_thr_a(t_list **stack_a, t_list **stack_b)
 {
 	while (ft_lstsize(*stack_a) > 3 && order_check(stack_a) == 0)
 	{
-		rot_check(stack_a, stack_b);
+		stack_check(stack_a);
 		pb(stack_b, stack_a);
+
+	}
+	if (order_check(stack_a) == 0)
+		sort_thr(stack_a);
+	while (*stack_b)
+	{
+		if (ft_lstsize(*stack_b) == 1)
+		{
+			pa(stack_a, stack_b);
+			*stack_b = NULL;
+			break ;
+		}
+		pa(stack_a, stack_b);
 	}
 }
 
 void	sort_stack(t_list **stack_a, t_list **stack_b)
 {
+	/*if (ft_lstsize(*stack_a) > 3 && order_check(stack_a) == 0)
+		pb(stack_b, stack_a);
+	if (ft_lstsize(*stack_a) > 3 && order_check(stack_a) == 0)
+		pb(stack_b, stack_a);*/
 	if (ft_lstsize(*stack_a) > 3 && order_check(stack_a) == 0)
 		push_b_thr_a(stack_a, stack_b);
 	if (ft_lstsize(*stack_a) == 3)
@@ -89,7 +151,8 @@ int	main(int ac, char **av)
 		ft_printf("b:\t");
 		while (stack_b)
 		{
-			ft_printf("%d\t", stack_b->content);
+			ft_printf("%d ", stack_b->content);
+			ft_printf("%d\t", stack_b->index);
 			stack_b = stack_b->next;
 		}
 		ft_printf("\n");
