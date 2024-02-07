dedd2e56dc14e0d0c52aba87f8b0aca772b3d457
donkeykang316 <kang_an_an@hotmail.de>
2024-02-07 22:47:44 +0100

    almost done

--- minitalk/client.c
+++ minitalk/client.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaan <kaan@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 21:31:24 by kaan              #+#    #+#             */
-/*   Updated: 2024/02/07 17:32:12 by kaan             ###   ########.fr       */
+/*   Updated: 2024/02/07 22:46:22 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,41 @@
 #include <signal.h>
 #include <unistd.h>
 
-static void	sig_handler(int signbr)
+void	send_bit(int pid, char **av)
 {
+	int	i;
+	int	x;
+	int	message;
+
+	i = 0;
+	while (av[2][i])
+	{
+		message = av[2][i];
+		x = 128;
+		while (x > 0)
+		{
+			if (message & x)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+			x /= 2;
+		}
+		i++;
+	}
 }
 
 int	main(int ac, char **av)
 {
-	signal(SIGUSR2, sig_handler);
-	ft_printf("Client PID [%d]\n", getpid());
-	ft_printf("Sending PID to server...\n");
+	int	pid;
+
+	if (ac == 3)
+	{
+		pid = ft_atoi(av[1]);
+		ft_printf("Client PID [%d]\n", getpid());
+		ft_printf("Sending PID to server...\n");
+		send_bit(pid, av);
+	}
+	else
+		ft_printf("Wrong input, enter Server PID and then then the message\n");
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaan <kaan@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 21:31:39 by kaan              #+#    #+#             */
-/*   Updated: 2024/02/07 17:27:44 by kaan             ###   ########.fr       */
+/*   Updated: 2024/02/07 22:32:54 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,46 @@
 #include <signal.h>
 #include <unistd.h>
 
+char	bits_to_char(char bits[])
+{
+	char	result;
+	int		i;
+
+	result = 0;
+	i = 0;
+	while (i < 8)
+	{
+		result |= (bits[i] - '0') << (7 - i);
+		i++;
+	}
+	return (result);
+}
+
 static void	sig_handler(int signbr)
 {
-	if (signbr == SIGUSR2)
+	static char	buffer[9];
+	static int	i = 0;
+
+	if (signbr == SIGUSR1)
+		buffer[i] = '0';
+	else
+		buffer[i] = '1';
+	i++;
+	if (i == 8)
 	{
-		ft_printf("\nsignal handler function called\n");
+		buffer[9] = '\0';
+		ft_printf("%c", bits_to_char(buffer));
+		i = 0;
 	}
 }
 
 int	main(void)
 {
 	signal(SIGUSR1, sig_handler);
+	signal(SIGUSR2, sig_handler);
 	ft_printf("Server PID [%d]\n", getpid());
 	while (1)
+	{
 		sleep(1);
+	}
 }
